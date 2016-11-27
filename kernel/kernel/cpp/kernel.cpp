181f423f8c44bf9e815fa5479f48f4c95c3e182d
aeyxa <kianedits@hotmail.com>
2016-11-27 07:35:44 -0800

    added grub multiboot.h file, needs more work

--- kernel/kernel/cpp/kernel.cpp
+++ kernel/kernel/cpp/kernel.cpp
@@ -6,17 +6,18 @@
 #include <Regalia/isr.h>
 #include <Regalia/irq.h>
 #include <Regalia/keyboard.h>
+#include <Regalia/multiboot.h>
 #include <Global/globals.h>
 
 #define ever ;;
 
-void kernel_main(void)
+void kernel_main(multiboot_info_t* mbd, unsigned int magic)
 {
   terminal << "Regalia\n\nLoading Operating System...\n\n\n";
 
   Regalia::GlobalDescriptorTable gdt;
   Regalia::InterruptDescriptorTable idt;
-  
+
   __asm__ __volatile__ ("sti");
 
   for(ever);
