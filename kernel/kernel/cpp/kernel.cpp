746aa2c006aa1c587c8d09847c065798435699ad
aeyxa <kianedits@Hotmail.com>
2016-12-04 21:41:36 -0800

    cleaned up kernel

--- kernel/kernel/cpp/kernel.cpp
+++ kernel/kernel/cpp/kernel.cpp
@@ -9,20 +9,18 @@
 #include <Regalia/multiboot.h>
 #include <Global/globals.h>
 
-#include <Experimental/terminal.h>
-
 #define ever ;;
 
 void kernel_main(multiboot_info_t* mbd, unsigned int magic)
 {
-  terminal << "Regalia\n\nLoading Operating System...\n\n\n" << 0x0A << "\n";
+  terminal << "Regalia\n\nLoading Operating System...\n\n\n";
 
   Regalia::GlobalDescriptorTable gdt;
   Regalia::InterruptDescriptorTable idt;
 
   __asm__ __volatile__ ("sti");
 
-  //mbd->mmap_addr;  // This is grub
+  //mbd->mmap_addr;
 
   for(ever);
 }
